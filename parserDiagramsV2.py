2ca14e4c82930186e0480c966d9309fa0884e1e6
Даниил Ковалёв <dakovalyov@miem.hse.ru>
2024-09-25 19:16:43 +0300

    add descriptions1

--- parserDiagramsV2.py
+++ parserDiagramsV2.py
@@ -7,13 +7,16 @@ import logging
 import sys
 
 
+# Набор функций, отвечающие за чтение и анализ данных из файлов
 class FileUtils:
+    # Функция проверки файл на то, что он табличного типа
     @staticmethod
     def is_valid_file(file_path):
         valid_extensions = ['.xlsx', '.xls', '.csv']
         _, ext = os.path.splitext(file_path)
         return ext in valid_extensions
 
+    # Функция загрузки данных из табличного документа
     @staticmethod
     def load_data(file_path, sheet_name=None):
         _, ext = os.path.splitext(file_path)
@@ -24,6 +27,7 @@ class FileUtils:
         else:
             raise ValueError(f"Формат файла {ext} не поддерживается.")
 
+    # Функция создания папки для сохранения png картинок сгенерированных диаграмм
     @staticmethod
     def create_unique_folder(base_folder='Graphics'):
         folder_name = base_folder
@@ -34,6 +38,7 @@ class FileUtils:
         os.makedirs(folder_name)
         return folder_name
 
+    # Функция отвечающая за создание error.log файлов
     @staticmethod
     def create_unique_log_file(base_log_file='errors.log'):
         log_file_name = base_log_file
@@ -44,10 +49,9 @@ class FileUtils:
         return log_file_name
 
 
-##########################################
-# data_processor.py
-
+# Набор функций, отвечающие за сбор данных
 class DataProcessor:
+    # Сбор читаемых листов из табличного документа
     @staticmethod
     def load_valid_sheets(file_path):
         _, ext = os.path.splitext(file_path)
@@ -69,8 +73,10 @@ class DataProcessor:
                     continue
             return valid_sheets
         else:
+            # В случае CSV файла нет листов
             return [None]
 
+    # Чтение двух параметров из файла parameters.txt
     @staticmethod
     def read_parameters_from_file(file_path):
         params = {}
@@ -84,7 +90,9 @@ class DataProcessor:
         return params
 
 
+# Набор функций отвечающих за генерацию диаграмм
 class DiagramConstructor:
+    # Нахождение значений превышающих стандартное отклонение и их сокращение
     @staticmethod
     def bar_adjust(value, threshold, max_non_outlier_value):
         adjusted_height = value
@@ -95,6 +103,7 @@ class DiagramConstructor:
                 white_cut = True
         return adjusted_height, white_cut
 
+    # Добавление "белых срезов" для сокращенных столбцов
     @staticmethod
     def add_white_section(bars, original_values, adjusted_values):
         for bar, original, (adjusted, white_cut) in zip(bars, original_values, adjusted_values):
@@ -104,6 +113,7 @@ class DiagramConstructor:
                 y = bar.get_height() - bar.get_height() * 0.05
                 plt.bar(x, white_height, bottom=y, width=bar.get_width(), color='white', edgecolor='none')
 
+    # Базовая функция генерации диаграмм
     @staticmethod
     def make_diagrams(sheet_name, file_path, folder_name, standard_deviation, show_original_values):
         df = FileUtils.load_data(file_path, sheet_name=sheet_name)
@@ -113,16 +123,24 @@ class DiagramConstructor:
         values_2023 = df.iloc[2:, 3].astype(float).values
         values_2024 = df.iloc[2:, 2].astype(float).values
 
-        non_zero_and_one_filter = ((values_2022 != 0).astype(int) + (values_2023 != 0).astype(int) + (values_2024 != 0).astype(int)) > 1
-        regions, values_2022, values_2023, values_2024 = [arr[non_zero_and_one_filter] for arr in [regions, values_2022, values_2023, values_2024]]
+        non_zero_and_one_filter = ((values_2022 != 0).astype(int) + (values_2023 != 0).astype(int) + (
+                    values_2024 != 0).astype(int)) > 1
+        regions, values_2022, values_2023, values_2024 = [arr[non_zero_and_one_filter] for arr in
+                                                          [regions, values_2022, values_2023, values_2024]]
 
         sorted_indices = values_2024.argsort()
-        regions, values_2022, values_2023, values_2024 = [arr[sorted_indices] for arr in [regions, values_2022, values_2023, values_2024]]
+        regions, values_2022, values_2023, values_2024 = [arr[sorted_indices] for arr in
+                                                          [regions, values_2022, values_2023, values_2024]]
 
-        valid_data_filter = ((values_2022 != 0).astype(int) + (values_2023 != 0).astype(int) + (values_2024 != 0).astype(int)) > 1
-        mean_2022, mean_2023, mean_2024 = [arr[valid_data_filter & (arr > 0)].mean() for arr in [values_2022, values_2023, values_2024]]
-        std_2022, std_2023, std_2024 = [arr[valid_data_filter & (arr > 0)].std(ddof=0) for arr in [values_2022, values_2023, values_2024]]
-        threshold_2022, threshold_2023, threshold_2024 = [mean + standard_deviation * std for mean, std in zip([mean_2022, mean_2023, mean_2024], [std_2022, std_2023, std_2024])]
+        valid_data_filter = ((values_2022 != 0).astype(int) + (values_2023 != 0).astype(int) + (
+                    values_2024 != 0).astype(int)) > 1
+        mean_2022, mean_2023, mean_2024 = [arr[valid_data_filter & (arr > 0)].mean() for arr in
+                                           [values_2022, values_2023, values_2024]]
+        std_2022, std_2023, std_2024 = [arr[valid_data_filter & (arr > 0)].std(ddof=0) for arr in
+                                        [values_2022, values_2023, values_2024]]
+        threshold_2022, threshold_2023, threshold_2024 = [mean + standard_deviation * std for mean, std in
+                                                          zip([mean_2022, mean_2023, mean_2024],
+                                                              [std_2022, std_2023, std_2024])]
 
         x = range(len(regions))
         width = 0.25
@@ -135,13 +153,15 @@ class DiagramConstructor:
         )
 
         adjusted_2022 = [DiagramConstructor.bar_adjust(v, threshold_2022, max_non_outlier_value) for v in values_2022]
-        bars_2022 = plt.bar([i - width for i in x], [adj[0] for adj in adjusted_2022], width=width, label='2022', color='#A5A5A5')
+        bars_2022 = plt.bar([i - width for i in x], [adj[0] for adj in adjusted_2022], width=width, label='2022',
+                            color='#A5A5A5')
 
         adjusted_2023 = [DiagramConstructor.bar_adjust(v, threshold_2023, max_non_outlier_value) for v in values_2023]
         bars_2023 = plt.bar(x, [adj[0] for adj in adjusted_2023], width=width, label='2023', color='#ED7D31')
 
         adjusted_2024 = [DiagramConstructor.bar_adjust(v, threshold_2024, max_non_outlier_value) for v in values_2024]
-        bars_2024 = plt.bar([i + width for i in x], [adj[0] for adj in adjusted_2024], width=width, label='2024', color='#5B9BD5')
+        bars_2024 = plt.bar([i + width for i in x], [adj[0] for adj in adjusted_2024], width=width, label='2024',
+                            color='#5B9BD5')
 
         DiagramConstructor.add_white_section(bars_2022, values_2022, adjusted_2022)
         DiagramConstructor.add_white_section(bars_2023, values_2023, adjusted_2023)
@@ -155,19 +175,33 @@ class DiagramConstructor:
 
         if show_original_values:
             for i in range(len(x)):
-                if (values_2023[i] > threshold_2023 and values_2023[i] > max_non_outlier_value) or (values_2023[i] > y_max_metric):
-                    plt.text(x[i], DiagramConstructor.bar_adjust(values_2023[i], threshold_2023, max_non_outlier_value)[0] + 3.5,
-                             f'{int(values_2023[i]):,}'.replace(',', ' '), ha='center', va='bottom', rotation=90, fontsize=5)
-                if (values_2022[i] > threshold_2022 and values_2022[i] > max_non_outlier_value) or (values_2022[i] > y_max_metric):
-                    plt.text(x[i] - width, DiagramConstructor.bar_adjust(values_2022[i], threshold_2022, max_non_outlier_value)[0] + 3.5,
-                             f'{int(values_2022[i]):,}'.replace(',', ' '), ha='right', va='bottom', rotation=90, fontsize=5)
-                if (values_2024[i] > threshold_2024 and values_2024[i] > max_non_outlier_value) or (values_2024[i] > y_max_metric):
-                    plt.text(x[i] + width, DiagramConstructor.bar_adjust(values_2024[i], threshold_2024, max_non_outlier_value)[0] + 3.5,
-                             f'{int(values_2024[i]):,}'.replace(',', ' '), ha='left', va='bottom', rotation=90, fontsize=5)
+                if (values_2023[i] > threshold_2023 and values_2023[i] > max_non_outlier_value) or (
+                        values_2023[i] > y_max_metric):
+                    plt.text(x[i], DiagramConstructor.bar_adjust(values_2023[i], threshold_2023, max_non_outlier_value)[
+                        0] + 3.5,
+                             f'{int(values_2023[i]):,}'.replace(',', ' '), ha='center', va='bottom', rotation=90,
+                             fontsize=5)
+                if (values_2022[i] > threshold_2022 and values_2022[i] > max_non_outlier_value) or (
+                        values_2022[i] > y_max_metric):
+                    plt.text(x[i] - width,
+                             DiagramConstructor.bar_adjust(values_2022[i], threshold_2022, max_non_outlier_value)[
+                                 0] + 3.5,
+                             f'{int(values_2022[i]):,}'.replace(',', ' '), ha='right', va='bottom', rotation=90,
+                             fontsize=5)
+                if (values_2024[i] > threshold_2024 and values_2024[i] > max_non_outlier_value) or (
+                        values_2024[i] > y_max_metric):
+                    plt.text(x[i] + width,
+                             DiagramConstructor.bar_adjust(values_2024[i], threshold_2024, max_non_outlier_value)[
+                                 0] + 3.5,
+                             f'{int(values_2024[i]):,}'.replace(',', ' '), ha='left', va='bottom', rotation=90,
+                             fontsize=5)
 
-        plt.axhline(y=mean_2022, color='#A5A5A5', linestyle='--', linewidth=0.8, label=f'Среднее за 2022: {int(round(mean_2022)):,}'.replace(',', ' '))
-        plt.axhline(y=mean_2023, color='#ED7D31', linestyle='--', linewidth=0.8, label=f'Среднее за 2023: {int(round(mean_2023)):,}'.replace(',', ' '))
-        plt.axhline(y=mean_2024, color='#5B9BD5', linestyle='--', linewidth=0.8, label=f'Среднее за 2024: {int(round(mean_2024)):,}'.replace(',', ' '))
+        plt.axhline(y=mean_2022, color='#A5A5A5', linestyle='--', linewidth=0.8,
+                    label=f'Среднее за 2022: {int(round(mean_2022)):,}'.replace(',', ' '))
+        plt.axhline(y=mean_2023, color='#ED7D31', linestyle='--', linewidth=0.8,
+                    label=f'Среднее за 2023: {int(round(mean_2023)):,}'.replace(',', ' '))
+        plt.axhline(y=mean_2024, color='#5B9BD5', linestyle='--', linewidth=0.8,
+                    label=f'Среднее за 2024: {int(round(mean_2024)):,}'.replace(',', ' '))
 
         plt.xticks(x, regions, rotation=90, ha='right', fontsize=6)
         plt.rcParams['text.antialiased'] = True
@@ -208,7 +242,8 @@ class MainApp:
             show_original_values = params.get('show_original_values', True)
 
             for sheet in valid_sheets:
-                DiagramConstructor.make_diagrams(sheet, file_path, folder_name, standard_deviation, show_original_values)
+                DiagramConstructor.make_diagrams(sheet, file_path, folder_name, standard_deviation,
+                                                 show_original_values)
         else:
             print("Нет валидных листов для обработки.")
 
@@ -216,6 +251,7 @@ class MainApp:
         print("parserDiagram_27uvs.")
 
 
+# Запуск программы
 if __name__ == '__main__':
     log_file_name = FileUtils.create_unique_log_file()
     logging.basicConfig(filename=log_file_name, level=logging.ERROR, format='%(asctime)s - %(levelname)s - %(message)s')
